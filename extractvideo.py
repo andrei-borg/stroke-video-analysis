9db24f9335c212763e63449bea34bef3541ff7c1
Rackzzzz <oskartryggvason@gmail.com>
2023-02-22 16:37:33 +0100

    got working classification using eigenface and NN

--- extractvideo.py
+++ extractvideo.py
@@ -7,17 +7,15 @@ import matplotlib.pyplot as plt
 def extract_frames(path_to,path_from):
     
     video = cv2.VideoCapture(path_from)
-    
+    vid, frame = video.read()
     curr_frame = 0
-    os.chdir(r"C:\Users\oskar\OneDrive\Dokument\repo\kandidat\frames")
-    while(curr_frame<100):
+    
+    while(vid):
         
         vid, frame = video.read()
-        
         if vid:
-            name = path_to + str(curr_frame) + '.jpg'
+            name = path_to + '\\frame' + str(curr_frame) + '.jpg'
             print('Creating: '+ name)
-            
             cv2.imwrite(name, frame)
             
             curr_frame += 1
@@ -29,15 +27,18 @@ def extract_frames(path_to,path_from):
     
 def delete_frames(path):
     os.chdir(path)
-    for i in range(744):
-        os.remove("frames"+str(i)+".jpg")
-    
+    for i in range(2600):
+        try:
+            os.remove("frame"+str(i)+".jpg")
+        except:
+            pass
 def obtain_face(img_path):
     faces = RetinaFace.extract_faces(img_path)
     for face in faces:
         plt.imshow(face)
         plt.show()
     return faces[0]
-extract_frames(r"C:\Users\oskar\OneDrive\Dokument\repo\kandidat\frames\frames2",r"stroke-video-analysis\video\108050fps-1.MP4")
+delete_frames(r"C:\Users\oskar\OneDrive\Dokument\repo\kandidat\frames")
+#extract_frames(r"C:\Users\oskar\OneDrive\Dokument\repo\kandidat\frames",r"C:\Users\oskar\OneDrive\Dokument\repo\kandidat\video\108050fps-1.MP4")
 #delete_frames("frames")
 
