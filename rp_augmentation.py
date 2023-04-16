1f9a94ff8ae97f7f0a207ebec51f9fffec6b636a
Andrei Borg <andrei.borg@hotmail.com>
2023-04-16 20:01:35 +0200

    Update rp_augmentation.py

--- rp_augmentation.py
+++ rp_augmentation.py
@@ -1,32 +1,36 @@
 import numpy as np
-import tensorflow as tf
+# import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
+import os
 
 gen = ImageDataGenerator(
-    rotation_range=10,
-    width_shift_range=0.1,
+    rotation_range=14,
+    width_shift_range=0.08,
     height_shift_range=0.1,
-    shear_range=0.15,
-    zoom_range=0.1,
+    shear_range=0.2,
+    zoom_range=0.07,
     horizontal_flip=True,
 )
 
-image_path = (
-    "rp_data/test/stroke/Viktor_weakR4.png"  # Change this target image to augment
-)
-image = np.expand_dims(plt.imread(image_path), 0)
+for filename in os.listdir("rp_data/train/stroke"):
+    if 'weak' in filename:
 
-aug_iter = gen.flow(
-    image,
-    save_to_dir="rp_data/test/stroke",
-    save_prefix="aug",
-    save_format="png",
-)
+        image_path = "rp_data/train/stroke/" + filename
+        
+        image = np.expand_dims(plt.imread(image_path), 0)
 
-aug_images = [
-    next(aug_iter)[0].astype(np.uint8)
-    for i in range(7)  # Change range(x) for generating x augmentations
-]
+        aug_iter = gen.flow(
+            image,
+            save_to_dir="rp_data/denna2",
+            save_prefix="aug",
+            save_format="png",
+        )
 
-print("Created augmented images for:", image_path)
+        aug_images = [
+            next(aug_iter)[0].astype(np.uint8)
+            for i in range(35)  # Change range(x) for generating x augmentations
+        ]
+
+        print("Created augmented images for:", filename)
+    
